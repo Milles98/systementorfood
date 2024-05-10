d0800641b4faff19573be05bc6f5f08f03d96b86
Mille Elfver <mille.elfver98@gmail.com>
2024-05-10 15:51:40 +0200

    Created component for navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css'
+import { Home } from '.Components/Home';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
